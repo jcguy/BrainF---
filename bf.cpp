db8c8fafd06d2b8a87350d8a6abb65990b109254
James Corder Guy <corder.guy@gmail.com>
2017-04-05 15:43:17 -0500

    Add comments, change EOF to NULL

--- bf.cpp
+++ bf.cpp
@@ -12,67 +12,95 @@ int main(int argc, char** argv) {
         std::cerr << "No file given";
     }
 
-    // Read file to f_buf
+    // Determine the size of the file
     std::ifstream fin (argv[1], std::ifstream::in);
     std::filebuf* pbuf = fin.rdbuf();
     std::size_t size = pbuf->pubseekoff(0, fin.end, fin.in);
     pbuf->pubseekpos(0, fin.in);
+
+    // Copy the file to an array
     char* f_buf = new char[size];
     pbuf->sgetn(f_buf, size);
     fin.close();
 
+    // Set up the tape
     unsigned char* tape = new unsigned char[ARRAY_SIZE];
     unsigned char* t_ptr = tape;
 
     std::stack<char*> brackets;
 
-    for (char* f_ptr = f_buf; *f_ptr != EOF; f_ptr++) {
+    for (char* f_ptr = f_buf; *f_ptr; f_ptr++) {
         switch (*f_ptr) {
             case '+':
                 (*t_ptr)++;
                 break;
+
             case '-':
                 (*t_ptr)--;
                 break;
+
             case '>':
                 t_ptr++;
                 break;
+
             case '<':
                 t_ptr--;
                 break;
+
             case '[':
                 if (*t_ptr == 0) {
+                    /* Skip to the matching ']' by keeping
+                     * track of how many '['s we've seen so
+                     * far, and stopping when we've seen
+                     * just as many ']'s
+                     */
+                    
+                    // We've seen one so far
                     f_ptr++;
                     for (int n = 1; n > 0; f_ptr++) {
                         if (*f_ptr == '[') n++;
                         else if (*f_ptr == ']') n--;
-                        else if (*f_ptr == EOF) {
+                        else if (!(*f_ptr)) {
+                            // We've reached the end of the file
                             std::cerr << "Syntax error: unmatched [\n";
                             goto QUIT;
                         }
                     }
                 } else {
+                    /* Remember this location so we can jump
+                     * back to it when we reach the matching ']'
+                     */
                     brackets.push(f_ptr);
                 }
                 break;
+
             case ']':
                 if (*t_ptr != 0) {
+                    // Attempt to jump back to the matching '['
                     if (brackets.size() == 0) {
                         std::cerr << "Syntax error: unmatched ]\n";
                         goto QUIT;
                     }
                     f_ptr = brackets.top();
                 } else {
+                    // Forget about the matching '['
                     brackets.pop();
                 }
                 break;
+
             case '.':
                 std::cout << *t_ptr;
                 break;
+
             case ',':
                 std::cin >> *t_ptr;
                 break;
+
             case '#':
+                /* Prints out the first PRINT_SIZE cells in the
+                 * tape, vertically, with an arrow pointing to
+                 * the current location of the pointer.
+                 */
                 for (int i = 0; i < PRINT_SIZE; i++) {
                     std::cout << "[" << (int) tape[i] << "]";
                     if (t_ptr - tape == i) {
@@ -82,8 +110,6 @@ int main(int argc, char** argv) {
                 }
                 std::cout << "\n";
                 break;
-            case '\0':
-                goto QUIT;
         }
     }
 
